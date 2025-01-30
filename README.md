83d3d8f69add505ba37feb52407f984dbaea24e7
Myriam <myriam.malki@hotmail.fr>
2025-01-30 16:49:33 +0100

    Diapo fini

--- README.md
+++ README.md
@@ -25,6 +25,9 @@ Brief ECF 1 - Quatre pattes a besoin d'une nouvelle vitrine
 - Intégration CSS (7H)
 - Consignes Javascript (3H ...)
     - Fomulaire complet !
+    - Une catégorie de don (boutton) fait en focus pas l'autre
+
+
 
 Ce qu'il reste à faire : 
 
@@ -32,6 +35,7 @@ Ce qu'il reste à faire :
 - Faire le responsive CSS en version mobile 
 - Terminer une première version du JS (estimation fin le 30/01 17:30)
     - Revoir les effet de bouton de la partie "DON"
+    - Réinitialiser ces bouton en js
     - Revoir le code du "QUIZZ"
     - Faire la partie Diaporama
 
--- indexB1.js
+++ indexB1.js
@@ -1,7 +1,7 @@
 
 
 
-
+// DON 
 
 let optcat1=false;
 let optcat2=false;
@@ -25,6 +25,44 @@ function submitForm() {
 }
 
 
+// Diapo : Histoires
+
+function change(photo){
+
+const pp=document.getElementById("luna");
+let h4=document.getElementById("h4");
+let histoire=document.getElementById("histoire");
+let mois1=document.getElementById("mois1")
+
+
+    if(photo===1){
+        pp.setAttribute('style','background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chien-luna.png)')
+        
+
+        h4.innerHTML="Luna"
+        histoire.innerHTML="Trouvée dans la rue avec une patte cassée, Luna a été soignée et a retrouvé sa joie de vivre. Elle coule maintenant des jours heureux dans sa nouvelle famille."
+        mois1.innerHTML="Janvier 2024"
+
+
+    }else if(photo===2){
+        pp.setAttribute('style','background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chat-papillon.png)')
+
+        h4.innerHTML="Oscar";
+        histoire.innerHTML="Abandonnée dans une forêt, Oscar était terrifié par les humains. Après des mois de patience et d’amour, il est devenu un chat confiant et affectueux."
+        mois1.innerHTML="Mars 2024"
+
+    }else if(photo===3){
+        pp.setAttribute('style','background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chien-max.png)')
+
+        h4.innerHTML="Max"
+        histoire.innerHTML="Sauvé d'un laboratoire, Max n'avait jamais connu l'amour. Aujourd'hui, il profite de sa liberté et fait le bonheur de sa famille d'accueil."
+        mois1.innerHTML="Février 2024"
+    }
+
+    pp.style.backgroundPosition="center"
+    pp.style.backgroundSize="cover"
+
+}
 
 
 
@@ -33,16 +71,6 @@ function submitForm() {
 
 
 // Test / Quizz
-
-function defaut(){
-    document.getElementById
-}
-
-//function correct(){
-
-
-
-//}
 
 function ok(){
 
