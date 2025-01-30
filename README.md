99e170f03001ea1aefe3d06237f5e42734785f71
Myriam <myriam.malki@hotmail.fr>
2025-01-30 14:41:46 +0100

    maj reamdme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # brief-vitrine
 Brief ECF 1 - Quatre pattes a besoin d'une nouvelle vitrine
+
+
+## Contenue du projet
+
+- 1 page principale
+    - dont 1 indexB1.html
+    - dont 4 feuille CSS
+    - dont 1 indexB1.js
+
+- 1 page contact
+    - dont 1 contactB1.html
+    - dont 1 contactB1.css
+    - dont 1 contactB1.js
+
+## Déroulé en 4 jours
+
+- Télecharger les images
+    - Les comprésser
+    - Les convertir en png
+- Télécharger les icônes
+- Réaliser la maquette sur figma (6H)
+- Intégration HTML seul (2H)
+- Intégration CSS (7H)
+- Consignes Javascript (3H ...)
+    - Fomulaire complet !
+
+Ce qu'il reste à faire : 
+
+- Revoir les détails du CSS
+- Faire le responsive CSS en version mobile 
+- Terminer une première version du JS (estimation fin le 30/01 17:30)
+    - Revoir les effet de bouton de la partie "DON"
+    - Revoir le code du "QUIZZ"
+    - Faire la partie Diaporama
+
+
--- indexB1.js
+++ indexB1.js
@@ -1,5 +1,78 @@
 
 
-function f1(){
+
+
+
+let optcat1=false;
+let optcat2=false;
+
+function opt(cat){
+    if (cat === 1) {
+        optcat1 = true;
+    } else if (cat === 2) {
+        optcat2 = true;
+    }
+}
+
+// Fonction pour gérer la soumission
+function submitForm() {
+    if (optcat1 && optcat2) {
+        alert('Merci.');
+        // Ici, vous pouvez envoyer les données ou effectuer d'autres actions
+    } else {
+        alert('Veuillez choisir une option dans chaque catégorie avant de soumettre.');
+    }
+}
+
+
+
+
+
+
+
+
+
+// Test / Quizz
+
+function defaut(){
+    document.getElementById
+}
+
+//function correct(){
+
+
+
+//}
+
+function ok(){
+
+let div = document.getElementById("reponse1");
+let contenu1 = document.getElementById("contenu1")
+
+        div.classList.add("vert");
+
+
+        if(contenu1.hasAttribute("hidden")){
+            document.getElementById("contenu1").removeAttribute("hidden")
+        }else{
+            document.getElementById("contenu1").setAttribute("hidden","")
+        }
+}
+
+function non(){
+
+    let div = document.getElementById("reponse1");
+    let contenu1 = document.getElementById("contenu1")
+
+    div.classList.add("rouge"); 
+
+    if(contenu1.hasAttribute("hidden")){
+        document.getElementById("contenu1").removeAttribute("hidden")
+        document.getElementById("c1").setAttribute("hidden","")
+    }else{
+        document.getElementById("contenu1").setAttribute("hidden","")
+    }
+
     
+
 }
