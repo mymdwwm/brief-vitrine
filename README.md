19f600eebcd5420dcd6137982c42e71af795b59c
Myriam <myriam.malki@hotmail.fr>
2025-02-03 12:07:24 +0100

    Rendu final.

--- README.md
+++ README.md
@@ -19,37 +19,60 @@ Brief ECF 1 - Quatre pattes a besoin d'une nouvelle vitrine
 - Télecharger les images
     - Les comprésser
     - Les convertir en png
+
 - Télécharger les icônes
+
 - Réaliser la maquette sur figma (6H)
+
 - Intégration HTML seul (2H)
+
 - Intégration CSS (7H)
+
 - Consignes Javascript (9H ...)
     - Fomulaire complet !
     - Diaporama satisfaisant
     - Première version du JS (estimation fin le 30/01 17:30 - fin 23:30)
     - Une catégorie de don (boutton) fait en focus pas l'autre
-- Début du responsive (10H)
+
+- Début du responsive (14H)
     - menu : ok
-    - hero : ok
+        menu déroulant fait sur la base du livre "oh my code, e parle en javascript. Sonia" et de recherhe internet.
+    - hero : ok-
+        En version mobile, l'image de fond crée un léger décalage de gauche à droite.
     - don : ok
+        Apparence des bouttons pourrait être amélioré.
     - mission : ok
+        Correct
     - diapo : ok
-    - quizz : ok-
+        Correct
+    - quizz : ok
+        Correct, l'aspect pourrait être amélioré.
+        Manque le score finale
     - témoignane : ok
-    - contact : ok--
+        Correct
+    - impact : ok-
+        Reste à ajouté un léger écart entre les lignes.
+    - footer : ok
+        Correct
+    - contact : ok-
+        Reste effet shadow
+    - favicon : ok
+
+- Révision globale (30min)
+
+                                Temps approximatif : 38 H 30
 
 
+## Nouveaux Élements en avascript à détaillé : 
+- QUIZZ : 
+    . document.CreateElement : Modification HTML , ajout    d'une balise ( ici image ) dont l'affcihage n'est pas instantané sur la page. Peut être visible avec innerHTML et .. 
+    . prepend() : Ajoute un élément avant un autre déja présent( ici une icone avant le paragrahe ) différent de . append() qui ajoute a la apres.
 
-Ce qu'il reste à faire : 
-
-- Revoir les détails du CSS
-    - Réinitialiser ces bouton en js
-    - Revoir les couleurs des reponses du "QUIZZ"
-
-- Faire le responsive CSS en version mobile 
-    - Footer en column full width pas fonctionnel
-    - Partie "Impact" à mettre en column (difficile)
-    
-    
+    .toggle() : alterne des états, ajout ou suppression.
 
 
+## Ce qu'il reste à faire : 
+    - score au quizz a afficher /3
+
+
+        Réalisation : Myriam Ouari
--- indexB1.js
+++ indexB1.js
@@ -1,93 +1,119 @@
 
 
-
 // DON 
 
-let optcat1=false;
-let optcat2=false;
-
-function opt(cat){
-    if (cat === 1) {
-        optcat1 = true;
-    } else if (cat === 2) {
-        optcat2 = true;
-    }
+let montantChoisi = null;
+let frequenceChoisie = null;
+// Montant du don
+function optMontant(montant) {
+    montantChoisi = montant;
 }
-
-// Fonction pour gérer la soumission
+// Fréquence du don
+function optFrequence(frequence) {
+    frequenceChoisie = frequence;
+}
+// Envoi du "formulaire" et affichage
 function submitForm() {
-    if (optcat1 && optcat2) {
-        alert('Merci.');
-        // Ici, vous pouvez envoyer les données ou effectuer d'autres actions
-    } else {
-        alert('Veuillez choisir une option dans chaque catégorie avant de soumettre.');
+    if (montantChoisi !== null && frequenceChoisie !== null) { // Corect ; Affiche le maessage avec détail du don
+        alert("Merci ! Vous avez choisi un don de " + montantChoisi + "€ " + frequenceChoisie + ".");
+    } else { // Incorect ; un élément manquant affiche le maessage
+        alert("Veuillez choisir un montant et une fréquence avant de soumettre.");
     }
 }
-
 
 // Diapo : Histoires
 
-function change(photo){
+function change(photo) {
+    // Déclaration des variables 
+    const pp = document.getElementById("luna");
+    let h4 = document.getElementById("h4");
+    let histoire = document.getElementById("histoire");
+    let mois1 = document.getElementById("mois1")
 
-const pp=document.getElementById("luna");
-let h4=document.getElementById("h4");
-let histoire=document.getElementById("histoire");
-let mois1=document.getElementById("mois1")
-
-
-    if(photo===1){
-        pp.setAttribute('style','background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chien-luna.png)')
-        
-
-        h4.innerHTML="Luna"
-        histoire.innerHTML="Trouvée dans la rue avec une patte cassée, Luna a été soignée et a retrouvé sa joie de vivre. Elle coule maintenant des jours heureux dans sa nouvelle famille."
-        mois1.innerHTML="Janvier 2024"
-
-
-    }else if(photo===2){
-        pp.setAttribute('style','background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chat-papillon.png)')
-
-        h4.innerHTML="Oscar";
-        histoire.innerHTML="Abandonnée dans une forêt, Oscar était terrifié par les humains. Après des mois de patience et d’amour, il est devenu un chat confiant et affectueux."
-        mois1.innerHTML="Mars 2024"
-
-    }else if(photo===3){
-        pp.setAttribute('style','background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chien-max.png)')
-
-        h4.innerHTML="Max"
-        histoire.innerHTML="Sauvé d'un laboratoire, Max n'avait jamais connu l'amour. Aujourd'hui, il profite de sa liberté et fait le bonheur de sa famille d'accueil."
-        mois1.innerHTML="Février 2024"
+    // Au choix sur la miniature luna elle reprend la place pp
+    if (photo === 1) {
+        pp.setAttribute('style', 'background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chien-luna.png)') // image de fond et ombre linéaire
+        h4.innerHTML = "Luna" // Titre remplacer
+        histoire.innerHTML = "Trouvée dans la rue avec une patte cassée, Luna a été soignée et a retrouvé sa joie de vivre. Elle coule maintenant des jours heureux dans sa nouvelle famille." // histoire remplacée
+        mois1.innerHTML = "Janvier 2024" // date remplacée
+        // Au choix sur la miniature oscar, elle prend la pp
+    } else if (photo === 2) {
+        pp.setAttribute('style', 'background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chat-papillon.png)')
+        h4.innerHTML = "Oscar";
+        histoire.innerHTML = "Abandonnée dans une forêt, Oscar était terrifié par les humains. Après des mois de patience et d’amour, il est devenu un chat confiant et affectueux."
+        mois1.innerHTML = "Mars 2024"
+        // Au choix sur la miniature max, elle prend la pp
+    } else if (photo === 3) {
+        pp.setAttribute('style', 'background:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(/imageB1/chien-max.png)')
+        h4.innerHTML = "Max"
+        histoire.innerHTML = "Sauvé d'un laboratoire, Max n'avait jamais connu l'amour. Aujourd'hui, il profite de sa liberté et fait le bonheur de sa famille d'accueil."
+        mois1.innerHTML = "Février 2024"
     }
-
-    pp.style.backgroundPosition="center"
-    pp.style.backgroundSize="cover"
+    // Alignement des images 
+    pp.style.backgroundPosition = "center"
+    pp.style.backgroundSize = "cover"
 
 }
 
+// TEST / QUIZZ
+// Tableau pour comparer les réponses
+let bonnesReponses = {
+    1: "Faux",
+    2: "Faux",
+    3: "Faux"
+};
+let scoreU = 0;
+// Fonction à deux pramètres: 
+function deroule(numeroQuestion, reponseUtilisateur) {
+    // Déclaration des variables 
+    let buttonsId = "buttons" + numeroQuestion;
+    let reponseId = "reponse" + numeroQuestion;
+    // sélection par id
+    let buttonsDiv = document.getElementById(buttonsId);
+    let reponseDiv = document.getElementById(reponseId);
+
+    let scoreDiv = document.getElementById("scoreDiv");
+
+    // Masque les boutons au click via css
+    buttonsDiv.style.display = 'none';
+    // Affiche la réponse à la place via css
+    reponseDiv.style.display = 'flex';
+
+    // Création d'une balise <img> pour placer l'icone
+    let img = document.createElement("img");
+    img.alt = "Résultat"; // Texte alternatif de la source
+    // Vérifie que la réponse coorect est celle choisi par l'u
+    if (reponseUtilisateur === bonnesReponses[numeroQuestion]) {
+        // Fond vert si correct
+        reponseDiv.style.backgroundColor = 'var(--quizzvrai)';
+        // Ajt image check
+        img.src = "assetB1/checkvr.svg"; // Image de validation 
+        scoreU++ // Incrémentation
+    } else {
+        // Fond rouge si incorrect
+        reponseDiv.style.backgroundColor = 'var(--quizzfaux)';
+        // Ajt image croix
+        img.src = "assetB1/xre.svg"; // Image d'erreur 
+    }
+
+    // PARTIE NON FONCTIONNEL
+    // Ajouter l'image au début de la réponse
+    reponseDiv.prepend(img);
+    scoreDiv.textContent = "Score : " + scoreU + " / 3";
+
+    // Afficher la div du score si elle est cachée
+    if (scoreDiv.style.display === "none") {
+        scoreDiv.style.display = "block";
+    }
+}
 
 
+// HEADER
 
-
-
-
-// Test / Quizz
-
-function deroule(buttonsId, reponseId) {
-    // Cacher uniquement les boutons
-    document.getElementById(buttonsId).style.display = 'none';
-    // Afficher la réponse
-    document.getElementById(reponseId).style.display = 'flex';
-
-    
-  }
-
-
-  // header
-
-  function toggleMenu() {
+function toggleMenu() {
+    // Déclaration des variables
     const menuContent = document.getElementById('menu');
     const body = document.body;
-
     // Basculer la classe 'show' sur le menu
     menuContent.classList.toggle('show');
 
@@ -98,6 +124,5 @@ function deroule(buttonsId, reponseId) {
         body.classList.remove('menu-open');
     }
 }
-
 // Attache l'événement à l'icône du menu
 document.getElementById('croix').onclick = toggleMenu;
